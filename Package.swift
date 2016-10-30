2ac2cb9a65f0e45c773d5c5ffe751a3f6e4ec602
Richard Wei <rxrwei@gmail.com>
2016-10-29 19:56:55 -0500

    Add static/dynamic library targets

--- Package.swift
+++ Package.swift
@@ -12,3 +12,18 @@ let package = Package(
         .Package(url: "https://github.com/rxwei/CCUDA", majorVersion: 1)
     ]
 )
+
+//#if os(macOS)
+let dylib = Product(
+    name: "CUDA",
+    type: .Library(.Dynamic),
+    modules: [ "CUDADriver", "CUDARuntime", "NVRTC", "CuBLAS" ]
+)
+products.append(dylib)
+let staticLib = Product(
+    name: "CUDA",
+    type: .Library(.Static),
+    modules: [ "CUDADriver", "CUDARuntime", "NVRTC", "CuBLAS" ]
+)
+products.append(staticLib)
+//#endif
