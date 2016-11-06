93a82a887b97efc11cfa889ef9b37181a9b1ee08
Richard Wei <rxrwei@gmail.com>
2016-11-05 20:06:42 -0500

    3.0-RELEASE compiler crash workaround

--- Sources/Warp/Kernel.swift
+++ Sources/Warp/Kernel.swift
@@ -6,9 +6,6 @@
 //
 //
 
-import class NVRTC.Program
-import class NVRTC.Compiler
-import struct CUDARuntime.Device
 import CUDADriver
 
 /// Kernel operators
@@ -17,13 +14,13 @@ public extension KernelArgument {
 
     public static func array<T>(_ array: inout DeviceArray<T>) -> KernelArgument {
         return array.withUnsafeMutableDevicePointer { ptr in
-            self.init(unsafeAddress: ptr.deviceAddress)
+            KernelArgument(unsafeAddress: ptr.deviceAddress)
         }
     }
 
     public static func reference<T>(_ value: inout DeviceValue<T>) -> KernelArgument {
         return value.withUnsafeMutableDevicePointer { ptr in
-            self.init(unsafeAddress: ptr.deviceAddress)
+            KernelArgument(unsafeAddress: ptr.deviceAddress)
         }
     }
 
